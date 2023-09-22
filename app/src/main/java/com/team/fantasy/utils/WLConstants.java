2feea03f09a9a64ea4ca93743f83a9de4942ae7c
Swagnik02 <swagnik1234@gmail.com>
2023-09-23 01:12:50 +0530

    added additional fields to WorldLine PG

--- app/src/main/java/com/team/fantasy/utils/WLConstants.java
+++ app/src/main/java/com/team/fantasy/utils/WLConstants.java
@@ -6,29 +6,46 @@ public class WLConstants {
     public static final boolean ENABLE_ABORT_RESPONSE = true;
     public static final boolean ENABLE_MER_TXN_DETAILS = true;
 
-    public static final String DEVICE_ID = "AndroidSH2";
-    public static final String TOKEN = "0b125f92d967e06135a7179d2d0a3a12e246dc0ae2b00ff018ebabbe747a4b5e47b5eb7583ec29ca0bb668348e1e2cd065d60f323943b9130138efba0cf109a9";
-    public static final String PAYMENT_MODE = "all";
-    public static final String MERCHANT_LOGO_URL = "https://www.paynimo.com/CompanyDocs/company-logo-vertical.png";
-    public static final String MERCHANT_ID = "L3348";
-    public static final String CURRENCY = "INR";
-    public static final String CONSUMER_ID = "c964634";
-    public static final String CONSUMER_MOBILE_NO = "9876543210";
-    public static final String CONSUMER_EMAIL_ID = "test@test.com";
-    public static final String TXN_ID = "1667804027874";
+    public static final String DEVICE_ID = "ANDROIDSH2"; // Use the correct device identifier
+    public static final String TOKEN = "0b125f92d967e06135a7179d2d0a3a12e246dc0ae2b00ff018ebabbe747a4b5e47b5eb7583ec29ca0bb668348e1e2cd065d60f323943b9130138efba0cf109a9"; // Replace with your token
+    public static final String PAYMENT_MODE = "all"; // Choose the appropriate payment mode
+    public static final String MERCHANT_LOGO_URL = "https://www.paynimo.com/CompanyDocs/company-logo-vertical.png"; // Provide your logo URL
+    public static final String MERCHANT_ID = "T1234"; // Replace with your merchant ID
+    public static final String CURRENCY = "INR"; // Choose the appropriate currency
+    public static final String CONSUMER_ID = "c9"; // Replace with the consumer ID if needed
+    public static final String CONSUMER_MOBILE_NO = "9876543210"; // Provide the consumer's mobile number
+    public static final String CONSUMER_EMAIL_ID = "test@test.com"; // Provide the consumer's email ID
+    public static final String TXN_ID = "m1tr2uyyw1"; // Replace with your transaction ID
 
     // Additional constants
-    public static final String TOTAL_AMOUNT = "YourTotalAmount";
-    public static final String ACCOUNT_NO = "YourAccountNumber";
-    public static final String DEBIT_START_DATE = "YourDebitStartDate";
-    public static final String DEBIT_END_DATE = "YourDebitEndDate";
-    public static final String MAX_AMOUNT = "YourMaxAmount";
-    public static final String AMOUNT_TYPE = "YourAmountType";
-    public static final String FREQUENCY = "YourFrequency";
-    public static final String CARD_NUMBER = "YourCardNumber";
-    public static final String EXP_MONTH = "YourExpiryMonth";
-    public static final String EXP_YEAR = "YourExpiryYear";
-    public static final String CVV_CODE = "YourCVVCode";
+    public static final String TOTAL_AMOUNT = "YourTotalAmount"; // Replace with the total amount
+    public static final String ACCOUNT_NO = "YourAccountNumber"; // Replace with the account number
+    public static final String DEBIT_START_DATE = "YourDebitStartDate"; // Replace with the debit start date
+    public static final String DEBIT_END_DATE = "YourDebitEndDate"; // Replace with the debit end date
+    public static final String MAX_AMOUNT = "YourMaxAmount"; // Replace with the max amount
+    public static final String AMOUNT_TYPE = "YourAmountType"; // Replace with the amount type
+    public static final String FREQUENCY = "YourFrequency"; // Replace with the frequency
+    public static final String CARD_NUMBER = "4111********1111"; // Replace with the card number
+    public static final String EXP_MONTH = "09"; // Replace with the expiry month
+    public static final String EXP_YEAR = "2022"; // Replace with the expiry year
+    public static final String CVV_CODE = "123"; // Replace with the CVV code
 
     // Define other constants as needed
 }
+
+
+//    public static final boolean ENABLE_EXPRESS_PAY = true;
+//    public static final boolean ENABLE_INSTRUMENT_DEREGISTRATION = true;
+//    public static final boolean ENABLE_ABORT_RESPONSE = true;
+//    public static final boolean ENABLE_MER_TXN_DETAILS = true;
+//
+//    public static final String DEVICE_ID = "AndroidSH2";
+//    public static final String TOKEN = "0b125f92d967e06135a7179d2d0a3a12e246dc0ae2b00ff018ebabbe747a4b5e47b5eb7583ec29ca0bb668348e1e2cd065d60f323943b9130138efba0cf109a9";
+//    public static final String PAYMENT_MODE = "all";
+//    public static final String MERCHANT_LOGO_URL = "https://www.paynimo.com/CompanyDocs/company-logo-vertical.png";
+//    public static final String MERCHANT_ID = "L3348";
+//    public static final String CURRENCY = "INR";
+//    public static final String CONSUMER_ID = "c964634";
+//    public static final String CONSUMER_MOBILE_NO = "9876543210";
+//    public static final String CONSUMER_EMAIL_ID = "test@test.com";
+//    public static final String TXN_ID = "1667804027874";
